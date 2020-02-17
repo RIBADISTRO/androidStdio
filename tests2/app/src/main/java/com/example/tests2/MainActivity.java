e293f48248e437bc750bad7b50ff4275096ac9eb
RIBADISTRO <ribali0.96@gmail.com>
2020-02-17 16:06:41 -0600

    Introduciento mas cambio en este archivo

--- tests2/app/src/main/java/com/example/tests2/MainActivity.java
+++ tests2/app/src/main/java/com/example/tests2/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.tests2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
     }
-}
+
